7577643fe343118503304f243bf2914e3e4b5efb
Pietyboy <118936047+Pietyboy@users.noreply.github.com>
2023-12-27 12:13:33 +0300

    Add files via upload

---
+++ Lab_1/Test/Test/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Girafee {
+    class Program {
+        static void Main(string[] args) {
+            var a = 40;
+            string b = "John";
+
+
+            Console.WriteLine("Hello I'm " + b);
+            Console.WriteLine("And I'm " + a + " years old");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab_1/test.cs
@@ -0,0 +1,9 @@
+
+
+namespace Test {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+}
